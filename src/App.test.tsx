fa3af6052b866568f9e34c7b6df1ae6115645a70
Deb Jagadeba <deb.jagadev@live.com>
2025-07-10 16:27:53 +1000

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
+// Mocking axios to avoid actual API calls
+jest.mock('axios');
+
+beforeEach(() => {
+  (axios.get as jest.Mock).mockResolvedValue({
+    data: {
+      _embedded: {
+        employeeList: [], // or mock employees as needed
+      },
+    },
+  });
+});
+
+test('Renders the company name', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = await screen.findByText(/Employees of Bini corporation/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('Employer form should present', async () => {
+  render(<App />);
+  const form = await screen.findByLabelText("Employee Form");
+  expect(form).toBeInTheDocument();
 });
